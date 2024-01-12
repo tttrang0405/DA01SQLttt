7b9d4ef576dc6aef6401ed06b9eec2b183470c45
tttrang0405 <143953130+tttrang0405@users.noreply.github.com>
2024-01-12 12:21:31 +0700

    buoi11.sql

--- buoi11.sql
+++ buoi11.sql
@@ -1,6 +1,8 @@
 --ex1
-
-
+SELECT Country.Continent, ROUND(AVG(City.Population))
+FROM Country
+JOIN City ON Country.Code = City.CountryCode
+GROUP BY Country.Continent;
 -- ex2
 SELECT 
 ROUND(SUM(CASE WHEN t.signup_action = 'Confirmed' THEN 1 ELSE 0 END)*1.0 / COUNT(t.signup_action),2)
@@ -17,14 +19,36 @@ ON activities.user_id = age.user_id
 WHERE activities.activity_type IN ('send', 'open') 
 GROUP BY age.age_bucket;
 --ex4
+SELECT customer_id 
+FROM customer_contracts AS cc
+JOIN products as p ON cc.product_id = p.product_id
+GROUP BY customer_id
+HAVING COUNT(p.product_category) = (SELECT
+COUNT(product_category) FROM products);
 --ex5
+SELECT 
+e.employee_id,
+e.name,
+COUNT(r.employee_id) AS reports_count,
+ROUND(AVG(r.age)) AS average_age
+FROM Employees AS e
+JOIN Employees AS r ON e.employee_id = r.reports_to
+GROUP BY e.employee_id, e.name
+HAVING COUNT(r.employee_id) > 0
+ORDER BY e.employee_id;
 --ex6
-SELECT p.product_name, o.unit 
-FROM   (SELECT product_id, 
-               Sum(unit) AS unit 
-        FROM   orders 
-        WHERE  order_date BETWEEN '2020-02-01' AND '2020-02-29' 
-        GROUP  BY product_id 
-        HAVING unit >= 100) o 
-       INNER JOIN products p 
-               ON o.product_id = p.product_id 
+SELECT
+p.product_name,
+SUM(o.unit) AS unit
+FROM Products as p
+LEFT JOIN
+Orders as o ON p.product_id = o.product_id
+WHERE o.order_date BETWEEN '2020-02-01' AND '2020-02-29' OR o.order_date IS NULL
+GROUP BY p.product_id, p.product_name
+HAVING SUM(o.unit) >= 100;
+--ex7
+SELECT p.page_id 
+FROM pages AS p
+LEFT JOIN page_likes AS pl ON p.page_id = pl.page_id
+WHERE pl.page_id IS NULL
+ORDER BY p.page_id ASC;
